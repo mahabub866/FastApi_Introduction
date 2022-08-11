8b7305358e6819a999b2b472476bf7bf3efb8889
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2022-08-11 18:06:32 +0600

    office work 8_11_22

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,14 @@
 
+
 from turtle import title
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status,Response,HTTPException
 from blog import models
 from blog import schemas
 from sqlalchemy.orm import Session
-
+from typing import List
 from blog.database import  SessionLocal, engine
 
-from blog.schemas import Blog
+from blog.schemas import Blog, ShowBlog,User,ShowBlogDetails
 
 app=FastAPI()
 
@@ -19,10 +20,56 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/create',status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog,db:Session=Depends(get_db)):
     new_blog=models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/blog',response_model=List[schemas.ShowBlogDetails])
+def all(db:Session=Depends(get_db)):
+    blogs=db.query(models.Blog).all()
+    return blogs
+
+@app.delete('/delete/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,response:Response,db:Session=Depends(get_db)):
+    
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'blog is not found {id}')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    
+    return {'Deleted Sucessufly'}
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'blog is not found {id}')
+    blog.update(request.dict())
+    # blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return  {'Updated Sucessufly'}
+
+@app.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog)
+def show(id,db:Session=Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog with id {id} is not availabe')
+        # response.status_code=status.HTTP_404_NOT_FOUND
+        # return {'details': f'Blog with id {id} is not availabe'}
+    return blog
+
+@app.post('/user')
+def create_user(request:schemas.User,db:Session=Depends(get_db)):
+    new_user=models.User(name=request.name,email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,27 @@
 
 from pydantic import BaseModel
+
+
 class Blog(BaseModel):
     title:str
     body:str
+
+class ShowBlog(BaseModel):
+    title: str
+    body:str
+    class Config():
+        orm_mode=True
+
+class ShowBlogDetails(BaseModel):
+    title: str
+    body:str
+    class Config():
+        orm_mode=True
+
+class User(BaseModel):
+    name: str
+    email:str
+    password:str
+    
+    class Config():
+        orm_mode=True
