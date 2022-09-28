fa58b392bbf4f7f8e8ad92b44a2488d4b11866e6
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2022-09-29 00:35:58 +0600

    ss

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 
+from datetime import datetime
+from email.policy import default
+from enum import unique
 from sqlalchemy import Column,Integer,String,ForeignKey
 from sqlalchemy.orm import relationship
-
+import sqlalchemy as sa
 from blog.database import Base
 
 class Blog(Base):
@@ -21,5 +24,17 @@ class User(Base):
     name=Column(String)
     email=Column(String)
     password=Column(String)
+
+    blogs = relationship("Blog", back_populates="creator")
+
+class UserExamp(Base):
+    __tablename__ = 'usersex'
+
+    id=sa.Column(sa.Integer,primary_key=True,index=True)
+    name=sa.Column(sa.String)
+    email=sa.Column(sa.String,unique=True)
+    create_date=sa.Column(sa.DateTime,default=datetime.now,index=True)
+    last_login=sa.Column(sa.DateTime,default=datetime.now,index=True)
+    password=sa.Column(sa.String)
 
     blogs = relationship("Blog", back_populates="creator")
